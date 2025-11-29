e98664fecd5ef142bfb9ffea45a7b44f6ba42324
unknown <kristi@scienceserves.org>
2025-11-28 19:32:44 -0500

    Finalizing all map and site fixes (bounds, auto-close, CSS, content)

---
+++ public/interactive-maps/msx_old/data/ProposedMSXCompressorStations_18.js
@@ -0,0 +1,10 @@
+var json_ProposedMSXCompressorStations_18 = {
+"type": "FeatureCollection",
+"name": "ProposedMSXCompressorStations_18",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ -90.563127863255588, 33.238899002744517 ] } },
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "Point", "coordinates": [ -89.548313019174273, 32.994684546785557 ] } },
+{ "type": "Feature", "properties": { "id": "3" }, "geometry": { "type": "Point", "coordinates": [ -88.800929800222249, 32.309341592938807 ] } }
+]
+}
